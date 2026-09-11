a06758adb9863c9a1c520b7e961a66ed172dae5d
K-Sarang-Nair <sarangk934@gmail.com>
2026-09-12 01:01:24 +0530

    updated

---
+++ .output/server/_tanstack-start-manifest_v-2ZtTAwdp.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-2ZtTAwdp.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/albin/OneDrive/Desktop/useless proj/Useless-Project-/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-Don088op.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-Don088op.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/albin/OneDrive/Desktop/useless proj/Useless-Project-/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-Bu0gb7UM.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
